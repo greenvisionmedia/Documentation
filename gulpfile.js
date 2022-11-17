8bb1228baa2126f157e0c4134868df51370dac19
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-17 13:10:57 -0700

    header/footer

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@ import markdown from 'gulp-markdown';
 import htmlmin from 'gulp-htmlmin';
 import rename from 'gulp-rename';
 import slugify from 'slugify';
+import header from 'gulp-header';
+import footer from 'gulp-footer';
+//import filter from 'gulp-filter';
+import fs from 'fs';
 
 function markup() {
     return gulp
         .src('./src/*.md')
-        .pipe(markdown())
+        .pipe(markdown())  
+        .pipe(header(fs.readFileSync('./src/header.html', 'utf8')))
+        .pipe(footer(fs.readFileSync('./src/footer.html', 'utf8')))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
         .pipe(
             rename(function (path) {
