1425f370beed5d9dbe4db66d1ce838658be85e1f
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-17 13:16:17 -0700

    pls

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@ import gulp from 'gulp';
 import markdown from 'gulp-markdown';
 import htmlmin from 'gulp-htmlmin';
 import rename from 'gulp-rename';
-import slugify from 'slugify';
 import header from 'gulp-header';
 import footer from 'gulp-footer';
-//import filter from 'gulp-filter';
 import fs from 'fs';
 
 function markup() {
@@ -15,11 +13,6 @@ function markup() {
         .pipe(header(fs.readFileSync('./src/header.html', 'utf8')))
         .pipe(footer(fs.readFileSync('./src/footer.html', 'utf8')))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(
-            rename(function (path) {
-                path.basename = slugify(path.basename, { lower: true });
-            })
-        )
         .pipe(gulp.dest('./public'));
 }
 
